6cc3dae66e346471a7a1b49f7bda9ada862cc842
1571221458@qq.com <majiafei>
2019-11-14 20:39:24 +0800

    首次提交

---
+++ src/main/java/com/sumnary/summary_11_14/Summary1114Application.java
@@ -0,0 +1,13 @@
+package com.sumnary.summary_11_14;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Summary1114Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Summary1114Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/sumnary/summary_11_14/Summary1114ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sumnary.summary_11_14;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Summary1114ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
